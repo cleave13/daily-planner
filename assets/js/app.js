8a607e307555f0b71a9d7dc29c886a803b0aa899
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-01-30 15:17:17 -0700

    completed jQuery and removed excess CSS classes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,12 @@
 //variables
-const textInputEls = $( 'input' );
-const saveBtnEl = $( '.fa-save' );
+
+const textInputEls = $('input');
+const saveBtnEl = $('.fa-save');
 const tableRowEls = $('tr');
+const tableInputEls = $('input');
 let savedEntries = [];
 
-console.log(tableRowEls);
 //functions
-
-function saveInput(event) {
-    const target = $( event.target );
-    const itemTime = $(target).parent().children().eq(0).text();
-    const itemInput = $(target).prev().children().eq(0).val();
-    const itemIndex = savedEntries.findIndex(item => item.time === itemTime);
-
-    const calendarItem = {
-        time: itemTime,
-        input: itemInput
-    };
-
-    if(itemIndex === -1) {
-        savedEntries.push(calendarItem);
-    } else {
-        savedEntries.splice(itemIndex, 1, calendarItem);  
-    }
-
-    localStorage.setItem('calendarItems', JSON.stringify(savedEntries));
-}
 
 function init() {
     const storedCalItems = JSON.parse(localStorage.getItem('calendarItems'));
@@ -41,11 +22,48 @@ function init() {
                     break;
                 }
             }
-            
+
         }
     }
+    styleRows();
     return;
-}
+};
+
+function styleRows() {
+    const currentHour = parseInt(moment().format("H"));
+    for (let i = 0; i < tableInputEls.length; i++) {
+        const inputRow = tableInputEls[i];
+        const inputHour = parseInt($(inputRow).attr('data-hour'));
+        console.log(inputHour);
+        if (inputHour === currentHour) {
+            $(inputRow).addClass('present');
+        } else if (inputHour < currentHour) {
+            $(inputRow).addClass('past');
+        } else {
+            $(inputRow).addClass('future');
+        }
+    }
+};
+
+function saveInput(event) {
+    const target = $(event.target);
+    const itemTime = $(target).parent().children().eq(0).text();
+    const itemInput = $(target).prev().children().eq(0).val();
+    const itemIndex = savedEntries.findIndex(item => item.time === itemTime);
+
+    const calendarItem = {
+        time: itemTime,
+        input: itemInput
+    };
+
+    if (itemIndex === -1) {
+        savedEntries.push(calendarItem);
+    } else {
+        savedEntries.splice(itemIndex, 1, calendarItem);
+    }
+
+    localStorage.setItem('calendarItems', JSON.stringify(savedEntries));
+};
 
 //special functions
 saveBtnEl.click(saveInput);
